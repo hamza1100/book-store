391e48cc36781b4d81204bd1283985fb51eea0f2
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-01-05 17:51:42 +0500

    creating/updating model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    # when print with Book.objects.all()
+    # it was displaying cryptic output
+    # override this class function to display
+    # in a human readable format
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
+    
+
+# to enter in shell
+    # python3 manage.py shell
+    # from book_outlet.models import Book
+    # Book.objects.all()/first()/last()
