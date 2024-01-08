33dd6017702b8d09dc211c8501c6d462b6f28584
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-01-08 18:38:01 +0500

    add slug db column and use in url

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('title')
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
     return render(request, 'book_outlet/index.html', {
-        'books': books
+        'books': books,
+        'total_number_of_books': num_books,
+        'average_rating': avg_rating
     })
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:    
     #     book_detail = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
-    book_detail = get_object_or_404(Book, pk=id)
+    book_detail = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_detail.html', {
         'title': book_detail.title,
         'author': book_detail.author,
